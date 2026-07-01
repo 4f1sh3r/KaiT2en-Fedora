f57a4017b174234ee472898ee8ef9b8dd0314e5a
Andre Eikmeyer <andre.eikmeyer@gmail.com>
2026-07-01 12:53:00 +0200

    installation: remove modules from forced loading in initramfs

--- scripts/fedora/rebuild-initramfs.sh
+++ scripts/fedora/rebuild-initramfs.sh
@@ -8,9 +8,8 @@ require_fedora
 require_command dracut
 
 KVER="$(kernel_release)"
-ADD_DRIVERS="t2smc t2bce t2bdrm t2hid t2touchbar_bl t2touchbar_kbd hid_t2magicmouse t2mfi_fastcharge t2gmux t2thunderbolt"
 
 info "rebuilding initramfs for $KVER"
-dracut --force --add-drivers "$ADD_DRIVERS" "/boot/initramfs-$KVER.img" "$KVER"
+dracut --force "/boot/initramfs-$KVER.img" "$KVER"
 
 info "initramfs rebuilt"
