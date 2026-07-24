72841efd0b67188b2ba3e64407dbabca1ed5901b
Andre Eikmeyer <dev@deq.rocks>
2026-07-24 17:28:42 +0200

    t2smp: order CPU hotplug with PM notifier priorities

--- modules/t2smp/t2smp.c
+++ modules/t2smp/t2smp.c
@@ -13,36 +13,25 @@
 
 #include <linux/cpu.h>
 #include <linux/cpumask.h>
-#include <linux/delay.h>
 #include <linux/module.h>
-#include <linux/mutex.h>
 #include <linux/notifier.h>
 #include <linux/pci.h>
 #include <linux/platform_data/x86/apple.h>
 #include <linux/suspend.h>
-#include <linux/workqueue.h>
 
 #define PCI_DEVICE_ID_APPLE_T2_BRIDGE 0x1801
-#define T2SMP_RESTORE_RETRIES 50
 
 static cpumask_var_t t2smp_offlined_cpus;
-static DEFINE_MUTEX(t2smp_lock);
-static unsigned int t2smp_restore_retries;
 
-static bool t2smp_restore_cpus(void)
+static void t2smp_restore_cpus(void)
 {
 	unsigned int cpu;
 	unsigned int restored = 0;
-	bool retry = false;
 	int ret;
 
 	for_each_cpu(cpu, t2smp_offlined_cpus) {
 		ret = add_cpu(cpu);
 		if (ret) {
-			if (ret == -EBUSY) {
-				retry = true;
-				continue;
-			}
 			pr_err("failed to restore CPU%u: %d\n", cpu, ret);
 			continue;
 		}
@@ -53,27 +42,6 @@ static bool t2smp_restore_cpus(void)
 
 	if (restored)
 		pr_info("restored %u secondary CPUs\n", restored);
-	return retry;
-}
-
-static void t2smp_restore_workfn(struct work_struct *work);
-static DECLARE_DELAYED_WORK(t2smp_restore_work, t2smp_restore_workfn);
-
-static void t2smp_restore_workfn(struct work_struct *work)
-{
-	bool retry;
-
-	mutex_lock(&t2smp_lock);
-	retry = t2smp_restore_cpus();
-	mutex_unlock(&t2smp_lock);
-
-	if (retry && t2smp_restore_retries) {
-		t2smp_restore_retries--;
-		schedule_delayed_work(&t2smp_restore_work,
-				      msecs_to_jiffies(20));
-	} else if (retry) {
-		pr_err("CPU hotplug remained busy; CPUs are still offline\n");
-	}
 }
 
 static void t2smp_offline_cpus(void)
@@ -82,15 +50,12 @@ static void t2smp_offline_cpus(void)
 	unsigned int offlined = 0;
 	int ret;
 
-	cancel_delayed_work_sync(&t2smp_restore_work);
-	mutex_lock(&t2smp_lock);
-
 	if (!cpumask_empty(t2smp_offlined_cpus)) {
 		pr_err("CPUs from the previous suspend remain offline\n");
 		t2smp_restore_cpus();
 		if (!cpumask_empty(t2smp_offlined_cpus)) {
 			pr_err("secondary CPU workaround skipped\n");
-			goto out_unlock;
+			return;
 		}
 	}
 
@@ -109,32 +74,38 @@ static void t2smp_offline_cpus(void)
 	}
 
 	pr_info("took %u secondary CPUs offline\n", offlined);
-
-out_unlock:
-	mutex_unlock(&t2smp_lock);
 }
 
-static int t2smp_pm_notify(struct notifier_block *nb, unsigned long action,
-			   void *unused)
+static int t2smp_prepare_notify(struct notifier_block *nb,
+				unsigned long action, void *unused)
 {
-	switch (action) {
-	case PM_SUSPEND_PREPARE:
+	if (action == PM_SUSPEND_PREPARE)
 		t2smp_offline_cpus();
-		break;
-	case PM_POST_SUSPEND:
-		t2smp_restore_retries = T2SMP_RESTORE_RETRIES;
-		schedule_delayed_work(&t2smp_restore_work, 0);
-		break;
-	default:
-		break;
-	}
 
 	return NOTIFY_OK;
 }
 
-static struct notifier_block t2smp_pm_notifier = {
-	.notifier_call = t2smp_pm_notify,
+static int t2smp_restore_notify(struct notifier_block *nb,
+				unsigned long action, void *unused)
+{
+	if (action == PM_POST_SUSPEND)
+		t2smp_restore_cpus();
+
+	return NOTIFY_OK;
+}
+
+/*
+ * The CPU core PM notifier runs at priority 0. Offline CPUs before it blocks
+ * hotplug, then restore them after it enables hotplug again.
+ */
+static struct notifier_block t2smp_prepare_notifier = {
+	.notifier_call = t2smp_prepare_notify,
 	.priority = 1,
+};
+
+static struct notifier_block t2smp_restore_notifier = {
+	.notifier_call = t2smp_restore_notify,
+	.priority = -1,
 };
 
 static int __init t2smp_init(void)
@@ -154,8 +125,15 @@ static int __init t2smp_init(void)
 	if (!alloc_cpumask_var(&t2smp_offlined_cpus, GFP_KERNEL))
 		return -ENOMEM;
 
-	ret = register_pm_notifier(&t2smp_pm_notifier);
+	ret = register_pm_notifier(&t2smp_prepare_notifier);
 	if (ret) {
+		free_cpumask_var(t2smp_offlined_cpus);
+		return ret;
+	}
+
+	ret = register_pm_notifier(&t2smp_restore_notifier);
+	if (ret) {
+		unregister_pm_notifier(&t2smp_prepare_notifier);
 		free_cpumask_var(t2smp_offlined_cpus);
 		return ret;
 	}
@@ -166,11 +144,9 @@ static int __init t2smp_init(void)
 
 static void __exit t2smp_exit(void)
 {
-	unregister_pm_notifier(&t2smp_pm_notifier);
-	cancel_delayed_work_sync(&t2smp_restore_work);
-	mutex_lock(&t2smp_lock);
+	unregister_pm_notifier(&t2smp_restore_notifier);
+	unregister_pm_notifier(&t2smp_prepare_notifier);
 	t2smp_restore_cpus();
-	mutex_unlock(&t2smp_lock);
 	free_cpumask_var(t2smp_offlined_cpus);
 }
 
