a7df876bacf8f547ac740d368ead0a4437c2711b
Andre Eikmeyer <dev@deq.rocks>
2026-09-15 15:11:18 +0200

    t2remote: move out of t2journal

--- scripts/fedora/install-apps.sh
+++ scripts/fedora/install-apps.sh
@@ -405,6 +405,7 @@ if [[ "$install_mode" == all ]]; then
 	install_rust_app "$REPO_ROOT/apps/t2-smc-control" "t2-smc-control"
 	install_rust_app "$REPO_ROOT/apps/t2-power-explorer" "t2-power-explorer"
 	install_rust_app "$REPO_ROOT/apps/t2-journal" "t2journal"
+	install_rust_app "$REPO_ROOT/apps/t2-remote" "t2-remote"
 	install_gpu_control
 	if ! "$REPO_ROOT/apps/t2-cpu-control/install.sh"; then
 		warn "t2-cpu-control installation failed; continuing"
